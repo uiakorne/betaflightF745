9cded83505317f1fcce315b8a2da81189f73dfe3
Kornelius <korneliusm@uia.no>
2023-05-02 14:02:36 +0200

    adding mixer_init to last config

--- src/main/flight/mixer_init.c
+++ src/main/flight/mixer_init.c
@@ -195,8 +195,8 @@ static const motorMixer_t mixerAtail4[] = {
 
 #if defined(USE_UNCOMMON_MIXERS)
 static const motorMixer_t mixerDualcopter[] = {
-    { 1.0f,  0.0f,  0.0f, -1.0f },          // LEFT
-    { 1.0f,  0.0f,  0.0f,  1.0f },          // RIGHT
+    { 1.0f,  0.0f,  0.0f, 0.0f },          // LEFT
+    { 0.0f,  0.0f,  0.0f,  1.0f },          // RIGHT
 };
 #else
 #define mixerDualcopter NULL
